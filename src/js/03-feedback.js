eb8f5d7984a88c698007e2cedd15ed2ea2d5c53f
D. Akelar <108392983+akelarka@users.noreply.github.com>
2022-12-01 21:29:05 +0200

    третє завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const KEY = 'feedback-form';
+
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textArea');
+
+let feedbackForm = {
+  email: '',
+  message: '',
+};
+
+function inputData() {
+  let existingData = localStorage.getItem(KEY);
+  if (existingData) {
+    existingData = JSON.parse(existingData);
+    Object.entries(existingData).forEach(([name, value]) => {
+      feedbackForm[name] = value;
+      form.elements[name].value = value;
+    });
+  }
+}
+inputData();
+
+const formInput = event => {
+  feedbackForm[event.target.name] = event.target.value;
+  localStorage.setItem(KEY, JSON.stringify(feedbackForm));
+};
+
+const submit = evt => {
+  evt.preventDefault();
+  const elements = { email: email.value, message: message.value };
+  console.log(elements);
+  email.value = '';
+  message.value = '';
+  feedbackForm = {};
+  localStorage.removeItem(KEY);
+
+  evt.currentTarget.reset();
+};
+form.addEventListener('input', throttle(formInput, 500));
+form.addEventListener('submit', submit);
