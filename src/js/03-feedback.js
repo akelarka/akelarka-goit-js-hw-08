c97b071ab188700c626f9fcafb52f99e67f81d45
D. Akelar <108392983+akelarka@users.noreply.github.com>
2022-12-02 12:07:23 +0200

    required

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,6 +6,9 @@ const form = document.querySelector('form');
 const email = document.querySelector('input');
 const message = document.querySelector('textArea');
 
+email.elements[0].setAttribute('required', '');
+message.elements[1].setAttribute('required', '');
+
 let feedbackForm = {
   email: '',
   message: '',
